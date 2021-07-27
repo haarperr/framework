4faddaf1b2e1819cdf25cb9a1950215f474a2aca
James Roll <Jameslroll@gmail.com>
2021-07-26 20:40:08 -0400

    User flag exports

--- resources/[core]/user/client/flags.lua
+++ resources/[core]/user/client/flags.lua
@@ -1,7 +1,15 @@
-function Main:HasFlag(...)
-	local user = self:GetUser()
-	if not user then return false end
-
-	return user:HasFlag(...)
+for _, funcName in ipairs({
+	"HasFlag",
+	"IsMod",
+	"IsAdmin",
+	"IsOwner",
+}) do
+	local func = User[funcName]
+	Main[funcName] = function(self, ...)
+		local user = self:GetUser()
+		if not user then return false end
+	
+		return func(user, ...)
+	end
+	Export(Main, funcName)
 end
-Export(Main, "HasFlag")
