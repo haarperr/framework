3c891a0a9edd86a56cb9db037f96167b73a6e9e9
James Roll <Jameslroll@gmail.com>
2021-08-05 02:09:58 -0400

    Misc: add movetocoords & distance

--- resources/[misc]/utils/client/misc.lua
+++ resources/[misc]/utils/client/misc.lua
@@ -1,3 +1,5 @@
+local cancelControls = { 22, 24, 25, 30, 31, 32, 33, 34, 35, 36 }
+
 -- local retval, didHit, hitCoords, surfaceNormal, materialHash, entity = Raycast()
 function Raycast(ignore)
 	local camCoords = GetFinalRenderedCamCoord()
@@ -54,4 +56,91 @@ function Delete(entity)
 			Citizen.Wait(50)
 		end
 	end)
+end
+
+function Distance(a, b)
+	if type(a) == "number" then
+		a = GetEntityCoords(a)
+	end
+	if type(b) == "number" then
+		b = GetEntityCoords(b)
+	end
+
+	return #(a - b)
+end
+
+function MoveToCoords(coords, heading, snap, timeout)
+	local ped = PlayerPedId()
+
+	-- Movement tasks.
+	TaskGoToCoordAnyMeans(ped, coords.x, coords.y, coords.z, 1.0, 0, 0, 786603, 0xbf800000)
+
+	Citizen.Wait(100)
+
+	local startTime = GetGameTimer()
+
+	while GetIsTaskActive(ped, 224) and (not timeout or GetGameTimer() - startTime < timeout) and Distance(ped, coords) > 0.3 do
+		for k, v in ipairs(cancelControls) do
+			if IsControlPressed(0, v) then
+				ClearPedTasks(ped)
+				ClearPedSecondaryTask(ped)
+				
+				return false
+			end
+		end
+
+		if snap and Distance(ped, coords) < 0.1 then
+			break
+		end
+
+		Citizen.Wait(0)
+	end
+
+	-- Smooth to coords.
+	if snap then
+		local start = GetEntityCoords(ped, true)
+		local target = vector3(coords.x, coords.y, start.z)
+		local iters = math.floor(math.min(Distance(ped, coords) / 1.0, 1.0) * 200)
+
+		for i = 1, iters + 1 do
+			local newCoords = start + (target - start) * (i / iters)
+			SetEntityCoordsNoOffset(ped, newCoords.x, newCoords.y, newCoords.z, true, false)
+			Citizen.Wait(0)
+		end
+	end
+	
+	-- No heading: done!
+	if not heading then
+		return true
+	end
+
+	-- Turning tasks.
+	local targetCoords = coords + FromRotation(vector3(0.0, 0.0, heading + 90))
+
+	TaskTurnPedToFaceCoord(ped, targetCoords.x, targetCoords.y, targetCoords.z, 5000)
+
+	Citizen.Wait(100)
+
+	startTime = GetGameTimer()
+
+	while (not timeout or GetGameTimer() - startTime < timeout) and Dot(GetEntityForwardVector(ped), targetCoords - coords) < 0.98 do
+		for k, v in ipairs(cancelControls) do
+			if IsControlPressed(0, v) then
+				ClearPedTasks(ped)
+				ClearPedSecondaryTask(ped)
+
+				return false
+			end
+		end
+
+		Citizen.Wait(0)
+	end
+
+	if snap then
+		Citizen.Wait(200)
+		ClearPedTasksImmediately(ped)
+		SetEntityHeading(ped, heading)
+	end
+
+	return true
 end
