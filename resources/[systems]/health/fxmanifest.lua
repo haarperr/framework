523d07af5115cae6986f047e77887352f15629e4
James Roll <Jameslroll@gmail.com>
2021-08-19 09:16:13 -0400

    Rebase health

--- resources/[systems]/health/fxmanifest.lua
+++ resources/[systems]/health/fxmanifest.lua
@@ -1,17 +1,13 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 
 ui_page 'html/index.html'
 
 files {
-	'html/images/*.png',
-	'html/**/*.html',
 	'html/**/*.css',
+	'html/**/*.html',
 	'html/**/*.js',
-}
-
-dependencies {
-	'main',
+	'html/**/*.png',
 }
 
 shared_scripts {
@@ -20,13 +16,8 @@ shared_scripts {
 
 client_scripts {
 	'cl_health.lua',
-	'cl_death.lua',
-	'cl_beds.lua',
-	'cl_effects.lua',
-	'cl_food.lua',
 }
 
 server_scripts {
 	'sv_health.lua',
-	'sv_beds.lua',
 }
