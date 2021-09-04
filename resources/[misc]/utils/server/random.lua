1b3243e9a0bbcb9a26d8031f7cf9139ae533badc
James Roll <Jameslroll@gmail.com>
2021-09-03 20:07:34 -0400

    Add Random util

---
+++ resources/[misc]/utils/server/random.lua
@@ -0,0 +1,31 @@
+RandomSeed = os.time()
+
+function UpdateSeed()
+	RandomSeed = math.random(2147483648)
+	math.randomseed(RandomSeed)
+end
+
+UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LowerCase = "abcdefghijklmnopqrstuvwxyz"
+Numbers = "0123456789"
+Symbols = "!@#$%&()*+-,./\\:;<=>?^[]{}"
+
+function GetRandomText(length, ...)
+	local args = {...}
+	local input = ""
+
+	for _, charSet in ipairs(args) do
+		input = input..charSet
+	end
+
+	local output = ""
+
+	for i = 1, length do
+		UpdateSeed()
+
+		local rand = math.random(#input)
+		output = output..input:sub(rand, rand)
+	end
+
+	return output
+end
