58f83d2b27f7bd4c2de49d5263dbef5f684b08cf
James Roll <Jameslroll@gmail.com>
2021-09-18 17:06:58 -0400

    Casino: slots n stuff

--- resources/[gameplay]/casino/fxmanifest.lua
+++ resources/[gameplay]/casino/fxmanifest.lua
@@ -4,14 +4,17 @@ game 'gta5'
 shared_scripts {
 	'sh_config.lua',
 	'sh_casino.lua',
+	'games/**/sh_*.lua',
 }
 
 client_scripts {
+	'@utils/client/misc.lua',
 	'cl_casino.lua',
-	'games/cl_*.lua',
+	'games/**/cl_*.lua',
 }
 
 server_scripts {
+	'@utils/server/random.lua',
 	'sv_casino.lua',
-	'games/sv_*.lua',
+	'games/**/sv_*.lua',
 }
--- resources/[gameplay]/casino/sh_casino.lua
+++ resources/[gameplay]/casino/sh_casino.lua
@@ -10,29 +10,11 @@ function Casino:Init()
 	end
 end
 
-function Casino:Update()
-	local props
-	for _, game in ipairs(self.games) do
-		if game.Update and (not game.nextUpdate or GetGameTimer() > game.nextUpdate) then
-			if not props then
-				props = exports.oldutils:GetObjects()
-			end
-			local delay = game:Update(props)
-			if delay then
-				game.nextUpdate = GetGameTimer() + delay
-			end
-		end
-	end
-end
-
 function Casino:Register(game)
 	table.insert(self.games, game)
 end
 
---[[ Threads ]]--
-Citizen.CreateThread(function()
-	while true do
-		Casino:Update()
-		Citizen.Wait(0)
-	end
+--[[ Events ]]--
+AddEventHandler("casino:start", function()
+	Casino:Init()
 end)
