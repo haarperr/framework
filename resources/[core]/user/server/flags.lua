95f8e34f6811bb0dd79f25a264076a5cde02048b
James Roll <Jameslroll@gmail.com>
2021-09-23 10:30:10 -0400

    User: fix flag invert mask

--- resources/[core]/user/server/flags.lua
+++ resources/[core]/user/server/flags.lua
@@ -15,7 +15,7 @@ function User:SetFlag(flag, value)
 	if value and value ~= 0 then
 		flags = flags | mask
 	else
-		flags = flags | (~mask)
+		flags = flags & (~mask)
 	end
 
 	self:Set("flags", flags)
