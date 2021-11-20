7cd2a1b3b6eed1976247f0f67f28d5fb2ae2d58f
James Roll <Jameslroll@gmail.com>
2021-11-20 14:57:18 -0500

    Overhaul discord rich presence

--- resources/[third-party]/discord-rp/server.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-	while true do
-		TriggerClientEvent("discord-rp:updatePlayers", -1, GetNumPlayerIndices(), GetConvarInt("sv_maxClients", 0))
-		Citizen.Wait(15000)
-	end
-end)
