281eecdf53eefe7009d94414a2ebe86dd18f22b0
James Roll <Jameslroll@gmail.com>
2021-11-23 02:54:31 -0500

    Remove admin-tools

--- resources/[gameplay]/admin-tools/sv_config.lua
+++
@@ -1,9 +0,0 @@
-ExpectedResponses = 60
-MinResponses = 10
-MaxResponses = 70
-Errors = {
-	["Alpha"] = "user didn't initialize",
-	["Beta"] = "claims power-level to be %s",
-	["Charlie"] = "expected "..ExpectedResponses.." updates but received %s - probably network error",
-	["Delta"] = "requesting admin-level information with power %s",
-}
--- resources/[gameplay]/admin-tools/sv_noclip.lua
+++
@@ -1,7 +0,0 @@
-Noclip = {}
-
-RegisterNetEvent("admin-tools:toggleNoclip")
-AddEventHandler("admin-tools:toggleNoclip", function(toggle)
-	local source = source
-	Noclip[source] = toggle
-end)
