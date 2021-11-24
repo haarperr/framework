eda25991c765628b17dbfcd009141f68f275b139
James Roll <Jameslroll@gmail.com>
2021-11-23 20:29:54 -0500

    Admin: freecam improvements/blip/focus

--- resources/[core]/admin/modules/freecam/cl_freecam.lua
+++ resources/[core]/admin/modules/freecam/cl_freecam.lua
@@ -10,6 +10,8 @@ function Freecam:Update()
 	local ped = PlayerPedId()
 	local vehicle = IsPedInAnyVehicle(ped) and GetVehiclePedIsIn(ped) or nil
 	local delta = GetFrameTime()
+	local isPaused = IsPauseMenuActive()
+	local hasFocus = _Get("hasFocus") or isPaused
 
 	-- Check vehicle.
 	if vehicle and GetPedInVehicleSeat(vehicle, -1) ~= ped then
@@ -17,12 +19,12 @@ function Freecam:Update()
 	end
 
 	-- Get input.
-	local lookX = GetDisabledControlNormal(0, 1) -- Mouse left/right.
-	local lookY = GetDisabledControlNormal(0, 2) -- Mouse up/down.
+	local lookX = hasFocus and 0.0 or GetDisabledControlNormal(0, 1) -- Mouse left/right.
+	local lookY = hasFocus and 0.0 or GetDisabledControlNormal(0, 2) -- Mouse up/down.
 
-	local horizontal = GetDisabledControlNormal(0, 30) -- Left/right.
-	local vertical = GetDisabledControlNormal(0, 31) -- Forward/back.
-	local height = (IsDisabledControlPressed(0, 205) and -1.0) or (IsDisabledControlPressed(0, 206) and 1.0) or 0.0
+	local horizontal = hasFocus and 0.0 or GetDisabledControlNormal(0, 30) -- Left/right.
+	local vertical = hasFocus and 0.0 or GetDisabledControlNormal(0, 31) -- Forward/back.
+	local height = hasFocus and 0.0 or (IsDisabledControlPressed(0, 205) and -1.0) or (IsDisabledControlPressed(0, 206) and 1.0) or 0.0
 
 	-- Calculate vectors.
 	local forward = FromRotation(self.rotation + vector3(0, 0, 90))
@@ -30,7 +32,7 @@ function Freecam:Update()
 	local up = Cross(forward, right)
 
 	-- Scrolling.
-	local scroll = ((IsDisabledControlPressed(0, 241) and 1.0) or (IsDisabledControlPressed(0, 242) and -1.0) or 0.0)
+	local scroll = hasFocus and 0.0 or ((IsDisabledControlPressed(0, 241) and 1.0) or (IsDisabledControlPressed(0, 242) and -1.0) or 0.0)
 	if IsDisabledControlPressed(0, 19) then -- Left alt.
 		-- Changing fov.
 		self.fov = math.min(math.max(self.fov - scroll * 10.0, 10.0), 100.0)
@@ -42,25 +44,22 @@ function Freecam:Update()
 
 	-- Get offsets.
 	local speed = math.pow(self.speed, 2.0) * 400.0 * (IsDisabledControlPressed(0, 209) and 2.0 or 1.0)
-	local rotation = self.rotation - vector3(lookY, 0.0, lookX) * 0.16 / delta
+	local rotation = self.rotation - vector3(lookY, 0.0, lookX) * 15.0
 	local coords = self.coords + (forward * -vertical + right * horizontal + up * height) * delta * speed
 
 	-- Following.
-	local isFollowing = self.follow and DoesEntityExist(self.follow)
-	if IsDisabledControlJustPressed(0, 251) then -- F.
-		if isFollowing then
+	if not hasFocus and IsDisabledControlJustPressed(0, 203) then -- Spacebar.
+		if self.follow then
 			self.follow = nil
 		else
 			local retval, didHit, hitCoords, surfaceNormal, materialHash, entity = Raycast()
-			if didHit then
+			if didHit and GetEntityType(entity) ~= 0 then
 				self.follow = entity
 			end
 		end
-
-		isFollowing = not isFollowing
 	end
 
-	if isFollowing then
+	if self.follow and DoesEntityExist(self.follow) then
 		local followCoords = GetEntityCoords(self.follow)
 		rotation = ToRotation(Normalize(followCoords - coords))
 		coords = coords + GetEntityVelocity(self.follow) * delta
@@ -77,9 +76,9 @@ function Freecam:Update()
 	SetFocusPosAndVel(coords.x, coords.y, coords.z, 0.0, 0.0, 0.0)
 
 	-- Other controls.
-	if IsDisabledControlJustPressed(0, 69) then -- Left click.
+	if not hasFocus and IsDisabledControlJustPressed(0, 69) then -- Left click.
 		self:Deactivate()
-	elseif IsDisabledControlJustPressed(0, 70) then -- Right click.
+	elseif not hasFocus and IsDisabledControlJustPressed(0, 70) then -- Right click.
 		SetEntityCoordsNoOffset(vehicle or ped, coords.x, coords.y, coords.z, true)
 		
 		if vehicle then
@@ -89,33 +88,65 @@ function Freecam:Update()
 		end
 	end
 
+	-- Update blip.
+	local blip = self.blip
+	if blip and DoesBlipExist(blip) then
+		SetBlipCoords(blip, coords.x, coords.y, coords.z)
+		SetBlipRotation(blip, math.floor(rotation.z))
+	end
+
 	-- Disable controls.
-	DisableAllControlActions(0)
+	for control = 0, 169 do DisableControlAction(0, control) end
+	for control = 257, 287 do DisableControlAction(0, control) end
+	for control = 329, 360 do DisableControlAction(0, control) end
 end
 
 function Freecam:Activate()
 	if self.active then return end
 
+	-- Update cache.
 	self.active = true
 	self.coords = GetFinalRenderedCamCoord()
 	self.rotation = GetFinalRenderedCamRot()
-
+	
+	-- Create camera.
 	self.camera = Camera:Create({
 		coords = self.coords,
 		rotation = self.rotation,
 		fov = self.fov,
 	})
-
+	
 	self.camera:Activate()
+	
+	-- Create blip.
+	local blip = AddBlipForCoord(self.coords)
+	self.blip = blip
+
+	SetBlipHiddenOnLegend(blip, true)
+	SetBlipScale(blip, 1.0)
+	SetBlipSprite(blip, 6)
+	SetBlipColour(blip, 1)
+	SetBlipAlpha(blip, 192)
 end
 
 function Freecam:Deactivate()
 	if not self.active then return end
-	self.active = false
 
+	-- Uncache.
+	self.active = false
+	self.follow = nil
+
+	-- Destroy camera.
 	if self.camera then
 		self.camera:Destroy()
 		self.camera = nil
+	end
+
+	-- Remove blip.
+	local blip = self.blip
+	if blip and DoesBlipExist(blip) then
+		RemoveBlip(blip)
+		self.blip = nil
 	end
 
 	-- Clear focus.
