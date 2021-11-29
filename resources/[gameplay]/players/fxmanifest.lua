911a413460af5f0062d7063328756faaea3f1a8b
James Roll <Jameslroll@gmail.com>
2021-11-29 04:38:38 -0500

    Add players with /me

---
+++ resources/[gameplay]/players/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+	'@grids/shared/grids.lua',
+	'sh_config.lua',
+}
+
+client_scripts {
+	'cl_players.lua',
+	'modules/**/cl_*.lua',
+}
+
+server_scripts {
+	'@utils/server/players.lua',
+	'sv_players.lua',
+	'modules/**/sv_*.lua',
+}
