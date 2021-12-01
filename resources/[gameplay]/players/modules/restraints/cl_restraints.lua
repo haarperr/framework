e3630f4742f293fe3aeb2d5c30312eb8bd9a2c34
James Roll <Jameslroll@gmail.com>
2021-12-01 03:41:07 -0500

    Players: restraint resisting n stuff

--- resources/[gameplay]/players/modules/restraints/cl_restraints.lua
+++ resources/[gameplay]/players/modules/restraints/cl_restraints.lua
@@ -18,9 +18,8 @@ function Restraints:Stop()
 	LocalPlayer.state.restrained = nil
 end
 
-function Restraints:Update()
+function Restraints:UpdateEmote()
 	if self.active and not self.emote then
-		print("play emote")
 		self.emote = exports.emotes:Play(self.anims.cuffed)
 	elseif not self.active and self.emote then
 		exports.emotes:Stop(self.emote)
@@ -28,16 +27,58 @@ function Restraints:Update()
 	end
 end
 
+function Restraints:UpdateState(delta)
+	if not self.active then return end
+
+	local ped = PlayerPedId()
+	local chance = 0.0
+
+	-- Sprinting.
+	if IsPedSprinting(ped) then
+		chance = chance + 0.25
+	elseif IsPedRunning(ped) then
+		chance = chance + 0.05
+	end
+
+	-- Stairs.
+	if not IsPedStill(ped) and GetPedConfigFlag(ped, 253) then
+		chance = chance * 2.0
+	end
+
+	-- Check fatigue.
+	local fatigue = exports.health:GetEffect("Fatigue") or 0.0
+	
+	-- Ragdoll chance.
+	local ragdoll = GetRandomFloatInRange(0.0, 1.0) < chance / delta * (fatigue * 0.8 + 0.2)
+	if ragdoll then
+		SetPedToRagdoll(ped, 1000, 0, 3, true, true, false)
+	end
+	
+	-- Ragdoll anims.
+	-- if IsPedRagdoll(ped) then
+	-- 	Citizen.Wait(200)
+	-- 	exports.emotes:Play({ Dict = "get_up@cuffed", Name = "front_to_default", Flag = 0 })
+	-- end
+end
+
 function Restraints:UseItem(item, slot)
 	local player, playerPed, playerDist = GetNearestPlayer()
 	if not player or playerDist > Config.MaxDist then return end
+	-- local player = PlayerId()
 	
 	TriggerServerEvent("players:restrain", slot.slot_id, GetPlayerServerId(player))
 end
 
 --[[ Events: Net ]]--
-RegisterNetEvent("players:restrain", function()
-	
+RegisterNetEvent("players:restrainFinish", function(name)
+	Restraints:Start(name or true)
+end)
+
+RegisterNetEvent("players:restrainBegin", function()
+	local success = exports.quickTime:Begin({ speed = 1.4, goalSize = 0.2 })
+	if success then
+		TriggerServerEvent("players:restrainResist")
+	end
 end)
 
 --[[ Events ]]--
@@ -58,14 +99,15 @@ end)
 --[[ Threads ]]--
 Citizen.CreateThread(function()
 	while true do
-		if IsControlJustPressed(0, 46) then
-			if Restraints.active then
-				Restraints:Stop()
-			else
-				Restraints:Start()
-			end
-		end
-		Restraints:Update()
+		Restraints:UpdateEmote()
 		Citizen.Wait(0)
+	end
+end)
+
+Citizen.CreateThread(function()
+	local delay = 200
+	while true do
+		Restraints:UpdateState(1000 / delay)
+		Citizen.Wait(delay)
 	end
 end)
--- resources/[gameplay]/players/modules/restraints/sv_restraints.lua
+++ resources/[gameplay]/players/modules/restraints/sv_restraints.lua
@@ -1,10 +1,30 @@
 Restraints = Restraints or {}
+Restraints.players = {}
 
 --[[ Functions: Restraints ]]--
 function Restraints:UseItem(source, target, slotId)
 	exports.emotes:PlayShared(source, target, Restraints.anims.cuffing)
+
+	self.players[target] = 1
 	
-	TriggerClientEvent("players:restrain", target)
+	TriggerClientEvent("players:restrainBegin", target)
+
+	Citizen.SetTimeout(3000, function()
+		Restraints:Finish(source, target)
+	end)
+
+	return true
+end
+
+function Restraints:Finish(source, target)
+	local state = self.players[target]
+	self.players[target] = nil
+	
+	if state == 2 then
+		return
+	end
+
+	TriggerClientEvent("players:restrainFinish", target)
 end
 
 --[[ Events: Net ]]--
@@ -13,9 +33,24 @@ RegisterNetEvent("players:restrain", function(slotId, target)
 
 	if type(slotId) ~= "number" or type(target) ~= "number" or target <= 0 then return end
 
-	Restraints:UseItem(source, target, slotId)
+	if Restraints:UseItem(source, target, slotId) then
+		exports.log:Add({
+			source = source,
+			target = target,
+			verb  = "restrained",
+		})
+	end
 end)
 
-RegisterNetEvent("players:resistRestrain", function()
-	local source = sourec
+RegisterNetEvent("players:restrainResist", function()
+	local source = source
+	
+	if Restraints.players[source] ~= 1 then return end
+	Restraints.players[source] = 2
+
+	exports.log:Add({
+		source = source,
+		verb = "resisted",
+		noun = "restraints",
+	})
 end)
