deca54ff11e713e7f17a91b723cb6a40b390d765
James Roll <Jameslroll@gmail.com>
2021-12-03 03:37:48 -0500

    Fix tire pop

--- resources/[systems]/vehicles/modules/cl_popping.lua
+++ resources/[systems]/vehicles/modules/cl_popping.lua
@@ -1,8 +1,12 @@
 function Main.update:Popping()
-	if not Materials or Speed < 70.0 * 0.621371 then return end
+	if not IsDriver or not Materials then return end
 
-	-- 34 = gravel track
-	-- 181 = rails
+	local min = 32.187
+	local max = 128.748
+
+	if GetRandomFloatInRange(0.0, 1.0) > math.max(Speed - min, 0.0) / (max - min) then
+		return
+	end
 
 	for wheelIndex, material in pairs(Materials) do
 		if material == 34 and not IsVehicleTyreBurst(CurrentVehicle, wheelIndex, true) then
