e1381dfedf83d8a00a3e340e304e672f2d91423f
James Roll <Jameslroll@gmail.com>
2021-12-04 18:07:04 -0500

    Ragdoll: fix & add state checks

--- resources/[misc]/ragdoll/cl_ragdoll.lua
+++ resources/[misc]/ragdoll/cl_ragdoll.lua
@@ -1,23 +1,48 @@
-IsRagdolled = false
+Main = {}
 
+--[[ Functions ]]--
+function Main:Toggle(value)
+	self.value = value
+end
+
+function Main:Update()
+	if not self.value then
+		Citizen.Wait(200)
+		return
+	end
+
+	local state = (LocalPlayer or {}).state
+	if not state then return end
+
+	local ped = PlayerPedId()
+	local ragdoll = IsPedRagdoll(ped) and not IsPedGettingUp(ped)
+
+	if not ragdoll and (
+		not IsPedFalling(ped) and
+		not state.restrained and
+		not state.immobile and
+		not state.carrier and
+		not state.following
+	) then
+		SetPedToRagdoll(ped, 2000, 1000, 0)
+	end
+end
+
+--[[ Threads ]]--
 Citizen.CreateThread(function()
 	while true do
-		if IsRagdolled then
-			SetPedToRagdoll(PlayerPedId(), 2000, 1000, 0)
-		else
-			Citizen.Wait(200)
-		end
-		
+		Main:Update()
 		Citizen.Wait(0)
 	end
 end)
 
+--[[ Commands ]]--
 RegisterCommand("+nsrp_ragdoll", function()
-	IsRagdolled = true
+	Main:Toggle(true)
 end, true)
 
 RegisterCommand("-nsrp_ragdoll", function()
-	IsRagdolled = false
+	Main:Toggle(false)
 end, true)
 
 RegisterKeyMapping("+nsrp_ragdoll", "Ragdoll", "keyboard", "o")
