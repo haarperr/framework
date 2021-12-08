2f046caa21a3e9f72e106d136a3466ff5f439c9b
James Roll <Jameslroll@gmail.com>
2021-12-07 20:31:33 -0500

    Users: fix updating identifiers w/ auto increment gaps

--- resources/[core]/user/server/users.lua
+++ resources/[core]/user/server/users.lua
@@ -12,9 +12,20 @@ function User:Create(data)
 			queryValues["@"..key] = value
 		end
 	end
+
+	-- Check Steam.
+	if not queryValues["@steam"] then
+		error(("missing steam past authentication (%s)"):format(json.encode(data.identifiers)))
+	end
 	
 	-- Update database.
-	exports.GHMattiMySQL:Query("INSERT INTO `users` SET "..queryString.." ON DUPLICATE KEY UPDATE "..queryString, queryValues)
+	exports.GHMattiMySQL:Query(([[
+		IF EXISTS (SELECT 1 FROM `users` WHERE `steam`=@steam) THEN
+			UPDATE `users` SET %s WHERE `steam`=@steam;
+		ELSE
+			INSERT INTO `users` SET %s;
+		END IF;
+	]]):format(queryString, queryString), queryValues)
 
 	-- Update fields.
 	local user = exports.GHMattiMySQL:QueryResult([[
