e6394304fbae02f1cf0932df6f37b362619b145e
jameslroll <jameslroll@gmail.com>
2021-12-14 23:17:56 -0500

    Remove helicam

--- resources/[third-party]/helicam/heli_server.lua
+++
@@ -1,8 +0,0 @@
--- FiveM Heli Cam.
--- Version 1.3 2017-06-12.
-
-RegisterServerEvent('heli:spotlight')
-AddEventHandler('heli:spotlight', function(state)
-	local serverID = source
-	TriggerClientEvent('heli:spotlight', -1, serverID, state)
-end)
