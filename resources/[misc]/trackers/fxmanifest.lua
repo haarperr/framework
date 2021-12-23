feca515d54b66249b808e72fab355ce1a07f4e7f
jameslroll <jameslroll@gmail.com>
2021-12-23 07:05:50 -0500

    Add trackers/blips system

--- resources/[misc]/trackers/fxmanifest.lua
+++ resources/[misc]/trackers/fxmanifest.lua
@@ -6,9 +6,13 @@ shared_scripts {
 }
 
 client_scripts {
+	'@utils/client/blips.lua',
+	'cl_groups.lua',
 	'cl_trackers.lua',
 }
 
 server_scripts {
+	'@utils/server/players.lua',
+	'sv_groups.lua',
 	'sv_trackers.lua',
 }
