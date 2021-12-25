2e2d01e7476e81212c66261c20b074d58c78bd5a
jameslroll <jameslroll@gmail.com>
2021-12-25 06:58:14 -0500

    Empty peds

--- resources/[gameplay]/peds/fxmanifest.lua
+++ resources/[gameplay]/peds/fxmanifest.lua
@@ -3,13 +3,12 @@ game 'gta5'
 
 shared_script {
 	'sh_config.lua',
-	-- 'sh_peds.lua',
 }
 
 client_scripts {
 	'cl_peds.lua',
 }
 
--- server_scripts {
--- 	'sv_peds.lua',
--- }
+server_scripts {
+	'sv_peds.lua',
+}
