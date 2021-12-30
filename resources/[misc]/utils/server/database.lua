d447280777c157d3c723beb326885272bc9be88b
jameslroll <jameslroll@gmail.com>
2021-12-30 15:56:12 -0500

    Fix WaitForTable scalar query result crashing server ?

--- resources/[misc]/utils/server/database.lua
+++ resources/[misc]/utils/server/database.lua
@@ -25,6 +25,17 @@ local types = {
 	["timestamp"] = "number",
 }
 
+local function IsServerStarting()
+	for i = 0, GetNumResources() - 1 do
+		local resource = GetResourceByFindIndex(i)
+		local state = GetResourceState(resource)
+		if state == "starting" then
+			return true
+		end
+	end
+	return false
+end
+
 function DescribeTable(table)
 	local output = {}
 	local columns = exports.GHMattiMySQL:QueryResult("DESCRIBE `"..table.."`")
@@ -87,17 +98,21 @@ function RunQuery(path)
 end
 
 function WaitForTable(table)
+	while IsServerStarting() do
+		Citizen.Wait(200)
+	end
+
 	local schema = GetConvar("mysql_schema", "")
 	if schema == "" then return end
 
-	while exports.GHMattiMySQL:QueryScalar([[
+	while exports.GHMattiMySQL:QueryResult([[
 		SELECT 1 FROM information_schema.tables
 		WHERE table_schema=@schema AND table_name=@table
 		LIMIT 1;
 	]], {
 		["@schema"] = schema,
 		["@table"] = table,
-	}) ~= 1 do
+	})[1] ~= 1 do
 		Citizen.Wait(200)
 	end
 end
