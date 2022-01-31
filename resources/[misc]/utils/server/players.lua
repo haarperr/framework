b2db3f47a07e95d99afb6f8067ac986f613bd37d
James Roll <Jameslroll@gmail.com>
2022-01-30 23:18:47 -0500

    Players: add WaitForCooldown

--- resources/[misc]/utils/server/players.lua
+++ resources/[misc]/utils/server/players.lua
@@ -3,6 +3,29 @@ PlayerUtil = {
 }
 
 --[[ Functions: PlayerUtil ]]--
+function PlayerUtil:WaitForCooldown(source, duration, update, id)
+	local cooldown = self.cooldowns[source]
+	local lastUpdate = cooldown and cooldown[id or 1]
+
+	if lastUpdate then
+		while (
+			self.cooldowns[source] and
+			math.floor(lastUpdate * 1000.0) == math.floor((cooldown[id or 1] or 0) * 1000.0) and
+			os.clock() - lastUpdate < duration
+		) do
+			Citizen.Wait(0)
+		end
+	end
+
+	local isValid = not lastUpdate or not cooldown or lastUpdate == cooldown[id or 1]
+
+	if update then
+		self:UpdateCooldown(source, id)
+	end
+
+	return isValid
+end
+
 function PlayerUtil:CheckCooldown(source, duration, update, id)
 	local cooldown = self.cooldowns[source]
 	local lastUpdate = cooldown and cooldown[id or 1]
