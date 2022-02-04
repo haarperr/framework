df07097a074e338393cc35e46ed2709dcccb78df
James Roll <Jameslroll@gmail.com>
2022-02-03 21:21:12 -0500

    Sound: add 3d export

--- resources/[misc]/sound/sv_sound.lua
+++ resources/[misc]/sound/sv_sound.lua
@@ -1,16 +1,5 @@
-AddEventHandler("playSound", function(...)
-	TriggerClientEvent("playSound", -1, ...)
-end)
-
-RegisterNetEvent("playSound3D", function(name)
+function PlaySound3D(source, name, volume, distance)
 	local source = source
-
-	-- Check cooldonw.
-	if not PlayerUtil:CheckCooldown(source, 1.0) then return end
-	PlayerUtil:UpdateCooldown(source)
-
-	-- Check input.
-	if type(name) ~= "string" or name:len() > 64 then return end
 
 	-- Get ped.
 	local ped = GetPlayerPed(source)
@@ -23,5 +12,30 @@ RegisterNetEvent("playSound3D", function(name)
 	-- Play sound.
 	print(("[%s] is playing sound: %s"):format(source, name))
 	
-	exports.players:Broadcast(source, "playSound3D", name, coords, 1.0, 0.3)
+	exports.players:Broadcast(source, "playSound3D", name, coords, volume or 1.0, distance or 0.3)
+end
+
+--[[ Events ]]--
+AddEventHandler("playSound", function(...)
+	TriggerClientEvent("playSound", -1, ...)
+end)
+
+--[[ Events: Net ]]--
+RegisterNetEvent("playSound3D", function(name)
+	local source = source
+
+	-- Check cooldonw.
+	if not PlayerUtil:CheckCooldown(source, 1.0) then return end
+	PlayerUtil:UpdateCooldown(source)
+
+	-- Check input.
+	if type(name) ~= "string" or name:len() > 64 then return end
+
+	-- Play sound.
+	PlaySound3D(source, name)
+end)
+
+--[[ Exports ]]--
+exports("PlaySound3D", function(...)
+	PlaySound3D(...)
 end)
