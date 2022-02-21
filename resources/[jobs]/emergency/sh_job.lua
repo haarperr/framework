01c7b100f25e1b9674b449aeffcdcadfcdfd621b
James Roll <Jameslroll@gmail.com>
2022-02-21 12:56:02 -0500

    Basic flags for pd/ems

--- resources/[jobs]/emergency/sh_job.lua
+++ resources/[jobs]/emergency/sh_job.lua
@@ -1,8 +1,24 @@
+PD_FLAGS = {
+	[1 << 1] = { Name = "FTO" },
+
+	[1 << 10] = { Name = "Helicopter" },
+	[1 << 11] = { Name = "Motorcycle" },
+	[1 << 12] = { Name = "Interceptor" },
+	[1 << 13] = { Name = "Marine" },
+	[1 << 14] = { Name = "Medical" },
+}
+
+EMS_FLAGS = {
+
+}
+
 exports.jobs:Register("lspd", {
 	Title = "Police",
 	Name = "LSPD",
 	Faction = "pd",
 	Group = "lspd",
+	-- Description = "Known for winning situations.",
+	Flags = PD_FLAGS,
 	Clocks = {
 		{ Coords = vector3(453.3004, -986.6588, 30.59658), Radius = 3.5 },
 		{ Coords = vector3(453.5949, -980.6509, 30.57985), Radius = 3.5 },
@@ -60,6 +76,7 @@ exports.jobs:Register("bcso", {
 	Name = "BCSO",
 	Faction = "pd",
 	Group = "bcso",
+	Flags = PD_FLAGS,
 	Clocks = {
 		{ Coords = vector3(1848.291, 3686.824, 34.27011), Radius = 2.0 },
 	},
@@ -78,6 +95,7 @@ exports.jobs:Register("sasp", {
 	Name = "SASP",
 	Faction = "pd",
 	Group = "sasp",
+	Flags = PD_FLAGS,
 	Clocks = {
 		{ Coords = vector3(1543.839, 828.1853, 77.66039), Radius = 3.0 },
 		{ Coords = vector3(1546.448, 826.5332, 77.65541), Radius = 3.0 },
@@ -89,14 +107,67 @@ exports.jobs:Register("sasp", {
 })
 
 exports.jobs:Register("paramedic", {
+	Title = "EMS",
 	Name = "Paramedic",
 	Faction = "ems",
 	Group = "paramedic",
+	Flags = EMS_FLAGS,
 	Clocks = {
 		{ Coords = vector3(305.0252, -600.062, 43.28405), Radius = 2.0 },
 	},
 	Tracker = {
 		Group = "emergency",
 		State = "ems",
+	},
+	Ranks = {
+		{
+			Name = "EMT",
+		},
+		{
+			Name = "Paramedic",
+		},
+		{
+			Name = "Senior Paramedic",
+		},
+		{
+			Name = "Advanced Paramedic",
+		},
+		{
+			Name = "Lieutenant",
+			Flags = (
+				Jobs.Permissions.CAN_HIRE_OR_FIRE |
+				Jobs.Permissions.CAN_SET_USER_FLAGS |
+				Jobs.Permissions.CAN_SET_USER_STATUS |
+				Jobs.Permissions.CAN_SET_USER_OTHER
+			)
+		},
+		{
+			Name = "Captain",
+			Flags = (
+				Jobs.Permissions.CAN_HIRE_OR_FIRE |
+				Jobs.Permissions.CAN_SET_USER_FLAGS |
+				Jobs.Permissions.CAN_SET_USER_STATUS |
+				Jobs.Permissions.CAN_SET_USER_OTHER |
+				Jobs.Permissions.CAN_SET_USER_RANK
+			)
+		},
+		{
+			Name = "Dead of Medicine",
+			Flags = (
+				Jobs.Permissions.CAN_HIRE_OR_FIRE |
+				Jobs.Permissions.CAN_SET_USER_FLAGS |
+				Jobs.Permissions.CAN_SET_USER_STATUS |
+				Jobs.Permissions.CAN_SET_USER_OTHER |
+				Jobs.Permissions.CAN_SET_USER_RANK
+			)
+		},
+		{
+			Name = "Assistant Chief",
+			Flags = Jobs.Permissions.ALL()
+		},
+		{
+			Name = "Chief",
+			Flags = Jobs.Permissions.ALL()
+		},
 	},
 })
