80faf58c4233a6bd2edad50c526641e49896fa22
Hikato <hikato@hikato.com>
2022-07-27 06:57:26 -0500

    (mhacking): add mhacking

---
+++ resources/[third-party]/mhacking/__resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page 'hack.html'
+
+client_scripts {
+  'mhacking.lua',
+  'sequentialhack.lua'
+}
+
+files {
+  'phone.png',
+  'snd/*.ogg',
+  'hack.html'
+}
