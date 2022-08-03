a1318cec52323839bee85069042eaafb7e123da2
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-03 02:31:26 +0100

    Convenience Stores

--- resources/[systems]/shops/shops/convenienceStores.lua
+++ resources/[systems]/shops/shops/convenienceStores.lua
@@ -48,3 +48,105 @@ RegisterShop("HARMONY_247", {
 		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
 	},
 })
+
+RegisterShop("STRAWBERRY_247", {
+	Name = "Strawberry 24/7",
+	Clerks = {
+		{
+			coords = vector4(24.49411, -1346.465, 29.49702, 265.8573),
+		},
+	},
+	Storage = {
+		Coords = vector3(25.74689, -1346.503, 29.49702),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
+
+RegisterShop("CLINTON_247", {
+	Name = "Clinton 24/7",
+	Clerks = {
+		{
+			coords = vector4(372.7724, 327.5338, 103.5664, 254.6205),
+		},
+	},
+	Storage = {
+		Coords = vector3(377.9404, 333.4799, 103.5664),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
+
+RegisterShop("ROCKFORD_247", {
+	Name = "Rockford 24/7",
+	Clerks = {
+		{
+			coords = vector4(-1820.467, 794.5576, 138.0908, 129.331),
+		},
+	},
+	Storage = {
+		Coords = vector3(-1828.583, 800.1725, 138.1664),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
+
+RegisterShop("SENORA_247", {
+	Name = "Senora 24/7",
+	Clerks = {
+		{
+			coords = vector4(2676.496, 3280.292, 55.24112, 329.0203),
+		},
+	},
+	Storage = {
+		Coords = vector3(2672.76, 3286.728, 55.24112),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
+
+RegisterShop("PALETO_247", {
+	Name = "Paleto 24/7",
+	Clerks = {
+		{
+			coords = vector4(1728.647, 6416.733, 35.03728, 239.1772),
+		},
+	},
+	Storage = {
+		Coords = vector3(1734.608, 6421.002, 35.03728),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
+
+RegisterShop("PALETO_247", {
+	Name = "Paleto 24/7",
+	Clerks = {
+		{
+			coords = vector4(1959.326, 3741.4, 32.34373, 300.6078),
+		},
+	},
+	Storage = {
+		Coords = vector3(1959.257, 3748.864, 32.34373),
+		Radius = 1.0,
+		Filters = Config.Filters.Convenience,
+	},
+	Containers = {
+		{ text = "Counter", radius = 0.2, coords = vector3(548.6461, 2669.562, 42.2) },
+	},
+})
