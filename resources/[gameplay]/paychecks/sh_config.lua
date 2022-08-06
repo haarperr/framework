90e5e9bba47dfaafbfe8de26ba55d1dda831fcc7
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-06 15:48:23 +0100

    Paychecks

---
+++ resources/[gameplay]/paychecks/sh_config.lua
@@ -0,0 +1,17 @@
+Config = {
+	Coords = vector3(-544.2745361328125, -197.3819122314453, 38.22690963745117),
+	Interval = 1, -- In minutes.
+	DefaultPay = 10, -- In dollars.
+	PayMultiplier = 4,
+	Messages = {
+		Employed = "Your paycheck is in!",
+		Unemployed = "Your welfare check is in!",
+		Received = "$%s has been deposited into your bank from your paycheck.",
+		Bank = "$%s on your paycheck.",
+	},
+	Markers = {
+		Text = "Collect",
+		DrawRadius = 5.0,
+		Radius = 2.5,
+	}
+}
