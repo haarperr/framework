0cc42f0cb52c1a2ce7c3c4d41789728e48b9737d
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-06 18:30:05 +0100

    Smeltry

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -58,3 +58,32 @@ Register("Fryer", {
 	},
 	Model = "prop_chip_fryer",
 })
+
+Register("Smeltry", {
+	Placement = "Floor",
+	NoCenter = true,
+	Station = {
+		Type = "Smeltry",
+		Auto = true,
+		Magnet = {
+			Offset = vector3(0, -1, 0),
+			Heading = 0.0,
+		},
+		Anim = {
+			In = {
+				Locked = true,
+				Sequence = {
+					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
+					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
+				},
+			},
+			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
+		},
+		Camera = {
+			Offset = vector3(-1.0, -1.0, 2.0),
+			Target = vector3(0.0, 0.0, 1.0),
+			Fov = 60.0,
+		},
+	},
+	Model = "v_ilev_found_cranebucket",
+})
