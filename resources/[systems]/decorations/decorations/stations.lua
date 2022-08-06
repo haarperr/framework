7315b78417bbaa7349d06acc62ef80eb38562886
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-06 18:34:18 +0100

    Jewel Crafting Stuff

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -87,3 +87,32 @@ Register("Smeltry", {
 	},
 	Model = "v_ilev_found_cranebucket",
 })
+
+Register("Jewel Bench", {
+	Placement = "Floor",
+	NoCenter = true,
+	Station = {
+		Type = "Jewel Bench",
+		Auto = true,
+		Magnet = {
+			Offset = vector3(0, -1, 0),
+			Heading = 0.0,
+		},
+		Anim = {
+			In = {
+				Locked = true,
+				Sequence = {
+					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
+					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
+				},
+			},
+			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
+		},
+		Camera = {
+			Offset = vector3(-1.0, -1.0, 2.0),
+			Target = vector3(0.0, 0.0, 1.0),
+			Fov = 60.0,
+		},
+	},
+	Model = "gr_prop_gr_bench_04b",
+})
