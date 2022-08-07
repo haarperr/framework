38944f8176fc02460cda7c2dee390d771f9be961
Hikato <hikato@hikato.com>
2022-08-06 19:15:24 -0500

    (dealership): disable clock on blips for now

--- resources/[jobs]/dealership/sh_malone.lua
+++ resources/[jobs]/dealership/sh_malone.lua
@@ -6,8 +6,7 @@ exports.jobs:Register("maloneandsons", {
 	-- Description = "Known for winning situations.",
 	--Flags = PD_FLAGS,
 	Clocks = {
-		{ Coords = vector3(453.3004, -986.6588, 30.59658), Radius = 3.5 },
-		{ Coords = vector3(453.5949, -980.6509, 30.57985), Radius = 3.5 },
+		--{ Coords = vector3(453.3004, -986.6588, 30.59658), Radius = 3.5 },
 	},
 	Tracker = {
 		Group = "dealer",
