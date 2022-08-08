a08a7d4a1db22e1f8534e109895f4a57dbaabc0d
Ructar <duckhamm@gmail.com>
2022-08-08 13:41:17 -0400

    Update set command | Limit 50 transaction history | Rebuild fix submit button

--- resources/[systems]/banking/scripts/sv_banking.lua
+++ resources/[systems]/banking/scripts/sv_banking.lua
@@ -38,7 +38,7 @@ function Set(source, account, key, value)
     if BankAccounts[account] then 
         BankAccounts[account][key] = value
     end
-    exports.GHMattiMySQL:QueryAsync("UPDATE bank_accounts SET "..key.." = "..key.." + "..value.." WHERE account_id = @account_id", {
+    exports.GHMattiMySQL:QueryAsync("UPDATE bank_accounts SET "..key.." = "..value.." WHERE account_id = @account_id", {
         ["@account_id"] = account
     })
     if source ~= nil then
@@ -51,7 +51,7 @@ function GetTransactions(account)
     if not account then return end
     local transactions = {}
 
-    transactions = exports.GHMattiMySQL:QueryResult("SELECT * from bank_accounts_transactions WHERE account_id = @account_id ORDER BY transaction_date DESC LIMIT 100", {
+    transactions = exports.GHMattiMySQL:QueryResult("SELECT * from bank_accounts_transactions WHERE account_id = @account_id ORDER BY transaction_date DESC LIMIT 50", {
         ["@account_id"] = account
     })
 
