926d0fbc4780d7f135fc5caaa8428a2bc1c7bb4f
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-09 08:14:56 +0100

    More BM Shizzle

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -762,3 +762,32 @@ Register("Sugar and Spice Station", {
 	},
 	Model = "prop_foodprocess_01",
 })
+
+Register("Bean Machine Station", {
+	Placement = "Floor",
+	NoCenter = true,
+	Station = {
+		Type = "Bean Machine Station",
+		Auto = true,
+		Magnet = {
+			Offset = vector3(0, -1, 0),
+			Heading = 0.0,
+		},
+		Anim = {
+			In = {
+				Locked = true,
+				Sequence = {
+					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
+					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
+				},
+			},
+			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
+		},
+		Camera = {
+			Offset = vector3(-1.0, -1.0, 2.0),
+			Target = vector3(0.0, 0.0, 1.0),
+			Fov = 60.0,
+		},
+	},
+	Model = "prop_foodprocess_01",
+})
---
+++ resources/[systems]/shops/shops/beanmachine.lua
@@ -0,0 +1,41 @@
+RegisterShop("BEAN_MACHINE", {
+	Name = "Bean Machine",
+	Storage = {
+		Coords = vector3(-627.4974, 226.0371, 82.68631),
+		Radius = 1.0,
+	},
+	Decorations = {
+		["bmprocessor_1"] = {
+			item = "Bean Machine Station",
+			invisible = true,
+			coords = vector3(-629.5922, 222.7952, 81.87758),
+			rotation = vector3(-17.93439, -0, -179.8138),
+		},
+	},
+	Containers = {
+		{
+			text = "Tray",
+			radius = 0.2,
+			coords = vector3(-634.3727, 235.4191, 82.30943),
+		},
+		{
+			text = "Pass-through",
+			radius = 0.5,
+			coords = vector3(-635.6472, 233.2834, 81.88647),
+			discrete = true,
+		},
+		{
+			text = "Drinks",
+			radius = 0.5,
+			coords = vector3(-636.8191, 234.463, 81.59695),
+			discrete = true,
+			width = 4,
+			height = 3,
+			filters = {
+				category = {
+					["Drink"] = true,
+				}
+			},
+		},
+	},
+})
