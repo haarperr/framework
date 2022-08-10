6347dcdaafbdae9208ff1fba7f5545a4e72cf8b2
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-10 11:10:51 +0100

    Farm Selling stuffs

---
+++ resources/[systems]/shops/shops/farmshops.lua
@@ -0,0 +1,15 @@
+RegisterShop("FARM_1", {
+    Name = "Farm Shop",
+    Storage = { 
+        Coords = vector3(0,0,0),
+        Radius = 1.0,
+    },
+    Decorations = {
+        ["farm_1"] = {
+            item = "Farm Sell",
+            invisible = true,
+            coords = vector3(-70.12484, 6256.312, 32.1092),
+            rotation = vector3(-27.69349, -0, 19.57428),
+        },
+    },
+})
