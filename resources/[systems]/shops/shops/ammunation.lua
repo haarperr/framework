f00762cabba8053e7bddc2f0f6f4f71dfc85521d
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-11 07:20:41 +0100

    Update ammunation.lua
    This is my rifle, this is my gun, this is for fighting, this is for fun.

--- resources/[systems]/shops/shops/ammunation.lua
+++ resources/[systems]/shops/shops/ammunation.lua
@@ -32,3 +32,179 @@ RegisterShop("AMMUN_1", {
         Filters = Config.Filters.Ammunation,
     },
 })
+
+RegisterShop("AMMUN_2", {
+    Name = "El Rancho Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(813.8557, -2155.137, 29.62051, 358.7384),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(812.9092, -2156.415, 29.61921),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_3", {
+    Name = "Vespucci Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(841.1216, -1029.956, 28.18901, 270.4936),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(840.0104, -1028.978, 28.19485),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_4", {
+    Name = "Spanish Ave Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(247.5806, -51.69143, 69.92525, 341.6832),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(246.3692, -52.44442, 69.94115),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_5", {
+    Name = "Blv Del Perro Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-1310.437, -394.4501, 36.70116, 348.2448),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-1310.932, -395.4563, 36.69582),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_6", {
+    Name = "Little Seoul Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-659.1914, -939.0927, 21.83489, 94.04611),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-657.9359, -939.9092, 21.82935),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_7", {
+    Name = "Great Ocean Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-3167.734, 1087.25, 20.83811, 158.0379),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-3166.772, 1088.139, 20.83874),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_8", {
+    Name = "Paleto Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-326.4261, 6081.561, 31.45494, 133.8755),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-325.2082, 6082.043, 31.45481),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_9", {
+    Name = "Paleto Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-326.4261, 6081.561, 31.45494, 133.8755),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-325.2082, 6082.043, 31.45481),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_10", {
+    Name = "Sandy Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(1697.811, 3757.677, 34.70467, 138.1695),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(1698.694, 3758.405, 34.70532),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_11", {
+    Name = "Route 68 Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(-1112.864, 2697.713, 18.55428, 133.1035),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(-1112.864, 2697.713, 18.55428),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
+
+RegisterShop("AMMUN_12", {
+    Name = "Route 68 Ammunation",
+    License = "weapons",
+    Clerks = {
+        {
+            coords = vector4(2564.815, 298.526, 108.7336, 270.1561),
+            model = "csb_jackhowitzer",
+        },
+    },
+    Storage = {
+        Coords = vector3(2563.606, 298.9532, 108.735),
+        Radius = 2.0,
+        Filters = Config.Filters.Ammunation,
+    },
+})
