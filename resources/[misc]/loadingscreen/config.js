0c47aba236bea24fe7fd77ee206d7fb20aee7ef2
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-11 23:19:58 +0100

    New Loading Screen

---
+++ resources/[misc]/loadingscreen/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 18250,
+    "imgInterval": 8000,
+    "music": false,
+    /* Insert the Youtube Video ID below */
+    "videoID": "jo4-FhqkNwQ",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 70,
+    "text": {
+        "title": "",
+        "link": "discord.gg/nonstoprp",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png']
+}
--- resources/[misc]/loadingscreen/fxmanifest.lua
+++ resources/[misc]/loadingscreen/fxmanifest.lua
@@ -1,14 +1,17 @@
+
 fx_version 'adamant'
 game 'gta5'
 
-files {
-	'html/index.css',
-	'html/index.html',
-	'html/index.js',
-	'html/fonts/*.woff',
-	'html/assets/background.jpg',
-	'html/assets/slices/*.png',
-}
-
-loadscreen 'html/index.html'
+loadscreen 'index.html'
 loadscreen_manual_shutdown 'yes'
+
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'config.js',
+    'app.js',
+    'config.css',
+    'imgs/bg0.png',
+    'imgs/logo.png'
+}
