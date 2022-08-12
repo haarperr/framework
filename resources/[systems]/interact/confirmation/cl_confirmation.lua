8be5677e5600d1bafa7e85b6c0b1df500f96cfd8
Hikato <hikato@hikato.com>
2022-08-12 03:37:58 -0500

    (interact): port sendconfirm

---
+++ resources/[systems]/interact/confirmation/cl_confirmation.lua
@@ -0,0 +1,28 @@
+local waitingFor = nil
+
+--[[ Functions ]]--
+Messages["confirm"] = function(source, message, value)
+	if waitingFor then return end
+
+	exports.mythic_notify:PersistentAlert("START", "confirm", "inform", value.message.."! Type <span style='color: rgb(128, 255, 128)'>/accept</span> or <span style='color: rgb(255, 0, 0)'>/deny</span> to complete.")
+	waitingFor = value.id
+end
+
+--[[ Commands ]]--
+RegisterCommand("accept", function()
+	if not waitingFor then return end
+
+	exports.mythic_notify:PersistentAlert("END", "confirm")
+	TriggerServerEvent("interaction:confirm", waitingFor, true)
+	
+	waitingFor = nil
+end)
+
+RegisterCommand("deny", function()
+	if not waitingFor then return end
+
+	exports.mythic_notify:PersistentAlert("END", "confirm")
+	TriggerServerEvent("interaction:confirm", waitingFor, false)
+	
+	waitingFor = nil
+end)
