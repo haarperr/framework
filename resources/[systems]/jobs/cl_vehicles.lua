ffdd9e4525414e532a6ed5a178c2712fe764000e
Hikato <hikato@hikato.com>
2022-08-15 00:45:53 -0500

    (jobs): add impound command

--- resources/[systems]/jobs/cl_vehicles.lua
+++ resources/[systems]/jobs/cl_vehicles.lua
@@ -95,3 +95,40 @@ function TrySpawn(spawn)
     TriggerServerEvent("vehicles:subscribe", Vehicle, true)
 	TriggerServerEvent("jobs:getKeys", Vehicle)
 end
+
+--[[ Commands ]]--
+RegisterCommand("impound", function()
+	local dist = 4.0
+    local state = LocalPlayer.state or {}
+    if state.immobile or state.restrained then return end
+	
+	if not exports.jobs:IsInEmergency("CanImpound") then
+		return
+	end
+
+	local vehicle = exports.oldutils:GetNearestVehicle()
+	if not DoesEntityExist(vehicle) then return end
+
+	local ped = PlayerPedId()
+	if #(GetEntityCoords(ped) - GetEntityCoords(vehicle)) > dist then return end
+
+	exports.mythic_progbar:Progress({
+		Anim = {
+			Dict = "amb@world_human_stand_mobile_fat@female@standing@call@idle_a",
+			Name = "idle_b",
+			Flag = 49,
+			DisableMovement = true,
+		},
+		Label = "Impounding...",
+		Duration = 15000,
+		UseWhileDead = false,
+		CanCancel = true,
+		Disarm = false,
+	}, function(wasCancelled)
+		if wasCancelled then return end
+
+		if not DoesEntityExist(vehicle) or #(GetEntityCoords(ped) - GetEntityCoords(vehicle)) > dist then return end
+
+		exports.oldutils:Delete(vehicle)
+	end)
+end)
