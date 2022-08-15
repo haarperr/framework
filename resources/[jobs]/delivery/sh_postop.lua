7799eb6f5c51106b1cb0fbde2eb35afc42ae9329
Hikato <hikato@hikato.com>
2022-08-15 00:12:35 -0500

    (deliveries): functioning sorta, but added

--- resources/[jobs]/delivery/sh_postop.lua
+++ resources/[jobs]/delivery/sh_postop.lua
@@ -3,12 +3,43 @@ exports.jobs:Register("postop", {
 	Name = "Post-Op",
 	Faction = "delivery",
 	Group = "postop",
-	Clocks = {
-		--{ Coords = vector3(453.3004, -986.6588, 30.59658), Radius = 3.5 },
+	IsPublic = true,
+	Delivery = {
+		Center = vector3(-101.2231674194336, -902.0380859375, 58.2816047668457),
+		Properties = {
+			Radius = 3000.0,
+			Max = 128,
+			Types = {
+				["motel"] = true,
+				["apartment"] = true,
+				["house"] = true,
+				["mansion"] = true,
+			},
+		},
+		-- Destinations = {
+		-- 	vector3(-294.1211242675781, -917.2229614257812, 31.08061790466309),
+		-- 	vector3(-294.03851318359375, -914.292724609375, 31.08061981201172),
+		-- 	vector3(-296.4261474609375, -913.3141479492188, 31.08061981201172),
+		-- 	vector3(-298.4041442871094, -915.0750732421876, 31.08061981201172),
+		-- 	vector3(-297.544189453125, -917.4437866210938, 31.08061981201172),
+		-- },
+		Pay = 100,
 	},
-	Tracker = {
-		Group = "delivery",
-		State = "postop",
+	Clocks = {
+		{ Coords = vector3(-232.45993041992188, -915.5792236328124, 32.3108024597168), Radius = 3.5 },
+	},
+	Vehicles = {
+		{
+			Rank = 0,
+			In = vector3(-228.3406982421875, -911.2813110351564, 32.31079483032226),
+			Model = "boxville4",
+			PrimaryColor = 0,
+			SecondaryColor = 0,
+			Coords = {
+				vector4(-211.29608154296875, -916.7569580078124, 29.29507255554199, 155.83604431152344),
+				vector4(-225.6054840087891, -893.7627563476562, 29.77957344055175, 251.0935516357422),
+			},
+		},
 	},
 	Ranks = {
 		{ Name = "Delivery Driver" },
--- resources/[jobs]/delivery/sh_waterpower.lua
+++ resources/[jobs]/delivery/sh_waterpower.lua
@@ -3,12 +3,34 @@ exports.jobs:Register("waterpower", {
 	Name = "Los Santos Department of Water & Power",
 	Faction = "delivery",
 	Group = "waterpower",
-	Clocks = {
-		--{ Coords = vector3(453.3004, -986.6588, 30.59658), Radius = 3.5 },
+	IsPublic = true,
+	Delivery = {
+		Center = vector3(128.63401794433597, -1790.023681640625, 117.05559539794922),
+		Properties = {
+			Radius = 500.0,
+			Max = 128,
+			Types = {
+				["apartment"] = true,
+				["house"] = true,
+			},
+		},
+		Pay = 100,
 	},
-	Tracker = {
-		Group = "delivery",
-		State = "waterpower",
+	Clocks = {
+		{ Coords = vector3(520.2094116210938, -1652.173828125, 29.5268497467041), Radius = 3.5 },
+	},
+	Vehicles = {
+		{
+			Rank = 0,
+			In = vector3(527.1103515625, -1652.636474609375, 29.320009231567383),
+			Model = "boxville",
+			PrimaryColor = 0,
+			SecondaryColor = 0,
+			Coords = {
+				vector4(523.2278442382812, -1653.056396484375, 29.306880950927734, 136.12002563476565),
+				vector4(521.0298461914062, -1664.4891357421875, 29.09666633605957, 41.45882797241211),
+			},
+		},
 	},
 	Ranks = {
 		{ Name = "Delivery Driver" },
