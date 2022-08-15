717074bfae754d8ec454060d28fac085f97ab6bf
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-15 13:22:27 +0100

    Mechanic Storages

---
+++ resources/[systems]/shops/shops/mechanicshops.lua
@@ -0,0 +1,94 @@
+RegisterShop("MECHANIC_SHOP", {
+	Name = "Mechanic Shop",
+	Storage = {
+		Coords = vector3(-1196.097, -904.3538, 13.99892),
+		Radius = 1.0,
+	},
+	Containers = {
+		{
+			text = "Storage",
+			radius = 0.6,
+			coords = vector3(550.3969, -168.4101, 54.80797),
+			discrete = true,
+			width = 4,
+			height = 20,
+			filters = {
+				category = {
+					["Vehicle Repair"] = true,
+                    ["Vehicle"] = true,
+                    ["Resource"] = true,
+                    ["Tool"] = true,
+                    ["Heist"] = true,
+				}
+			},
+		},
+        {
+			text = "Storage",
+			radius = 0.6,
+			coords = vector3(952.424, -976.8836, 39.84251),
+			discrete = true,
+			width = 4,
+			height = 20,
+			filters = {
+				category = {
+					["Vehicle Repair"] = true,
+                    ["Vehicle"] = true,
+                    ["Resource"] = true,
+                    ["Tool"] = true,
+                    ["Heist"] = true,
+				}
+			},
+		},
+        {
+			text = "Storage",
+			radius = 0.6,
+			coords = vector3(547.6008, -169.6181, 54.49321),
+			discrete = true,
+			width = 4,
+			height = 20,
+			filters = {
+				category = {
+					["Vehicle Repair"] = true,
+                    ["Vehicle"] = true,
+                    ["Resource"] = true,
+                    ["Tool"] = true,
+                    ["Heist"] = true,
+				}
+			},
+		},
+        {
+			text = "Storage",
+			radius = 0.6,
+			coords = vector3(547.6008, -169.6181, 54.49321),
+			discrete = true,
+			width = 4,
+			height = 20,
+			filters = {
+				category = {
+					["Vehicle Repair"] = true,
+                    ["Vehicle"] = true,
+                    ["Resource"] = true,
+                    ["Tool"] = true,
+                    ["Heist"] = true,
+				}
+			},
+		},
+        {
+			text = "Storage",
+			radius = 0.6,
+			coords = vector3(547.6008, -169.6181, 54.49321),
+			discrete = true,
+			width = 4,
+			height = 20,
+			filters = {
+				category = {
+					["Vehicle Repair"] = true,
+                    ["Vehicle"] = true,
+                    ["Resource"] = true,
+                    ["Tool"] = true,
+                    ["Heist"] = true,
+				}
+			},
+		},
+	},
+})
