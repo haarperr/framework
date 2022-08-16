5ec0fc939f7a8708e9a4c71e2f57f3939412143f
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-16 05:54:15 +0100

    Update sh_config.lua

--- resources/[legacy]/paychecks/sh_config.lua
+++ resources/[legacy]/paychecks/sh_config.lua
@@ -1,7 +1,7 @@
 Config = {
 	Coords = vector3(-544.0047, -197.2489, 38.22694),
 	Radius = 1.5,
-	Interval = 15, -- In minutes.
+	Interval = 30, -- In minutes.
 	DefaultPay = 2, -- In dollars.
 	PayMultiplier = 1,
 	Messages = {
