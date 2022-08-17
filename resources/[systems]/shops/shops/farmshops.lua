43a9c22111ca48dbf5820ce72b2a646456c5720e
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-17 15:38:13 +0100

    ..and you're a dumbass

--- resources/[systems]/shops/shops/farmshops.lua
+++ resources/[systems]/shops/shops/farmshops.lua
@@ -2,13 +2,13 @@ RegisterShop("FARM_1", {
     Name = "Farm Shop",
     Storage = { 
         Coords = vector3(0,0,0),
-        Radius = 1.0,
+        Radius = 0.5,
     },
     Decorations = {
         ["farm_1"] = {
             item = "Farm Sell",
             invisible = true,
-            coords = vector3(-70.12484, 6256.312, 32.1092),
+            coords = vector3(-68.71407, 6256.855, 32.08312),
             rotation = vector3(-27.69349, -0, 19.57428),
         },
         ["egg_1"] = {
