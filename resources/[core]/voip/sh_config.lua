8eb9c8a50143b42d25678cc54846f2d911696731
Hikato <hikato@hikato.com>
2022-08-19 00:52:43 -0500

    (voip): update stages

--- resources/[core]/voip/sh_config.lua
+++ resources/[core]/voip/sh_config.lua
@@ -38,13 +38,12 @@ Config = {
 			{ Coords = vector3(-550.8392944335938, 281.9425048828125, 82.97657012939453), Radius = 1.5 }, -- Tequi-la-la L.
 			{ Coords = vector3(-551.5172729492188, 284.0843505859375, 82.97657012939453), Radius = 1.5 }, -- Tequi-la-la M.
 			{ Coords = vector3(-550.9138793945312, 286.9436950683594, 82.97657012939453), Radius = 1.5 }, -- Tequi-la-la R.
-			{ Coords = vector3(-454.537841796875, 271.7487487792969, 83.62382507324219), Radius = 3.5 }, -- Comedy Club.
-			{ Coords = vector3(-312.4431762695313, 6264.5966796875, 32.06183624267578), Radius = 1.5 }, -- Hen House.
 			{ Coords = vector3(-1831.1636962890625, -1190.7034912109375, 19.87976264953613), Radius = 1.5 }, -- Pearls.
-			{ Coords = vector3(-300.38397216796875, 197.457763671875, 100.85525512695312), Radius = 0.5 }, -- Blush Bunnies.
-			{ Coords = vector3(253.56578063964844, -418.2116088867188, 47.93564987182617), Radius = 1.3 }, -- Courthouse Judge seat.
-			{ Coords = vector3(255.65206909179688, -418.90777587890625, 47.71468734741211), Radius = 0.5 }, -- Courthouse The other seat valeyard wanted.
-			{ Coords = vector3(-1381.7760009765625, -614.6588745117188, 31.49820518493652), Radius = 1.5 }, -- Bahama Mamas.
+			{ Coords = vector3(-576.8135986328125, -210.30194091796875, 38.98944854736328), Radius = 1.5 }, -- Courthouse Judge seat.
+			{ Coords = vector3(-572.6636962890625, -207.92184448242188, 38.7354736328125), Radius = 0.5 }, -- Courthouse Speaking Stand.
+			{ Coords = vector3(-575.6636352539062, -212.1642608642578, 39.02237701416015), Radius = 0.8 }, -- Courthouse Witness Stand Right.
+			{ Coords = vector3(-577.8472900390625, -208.38658142089844, 38.66215896606445), Radius = 0.8 }, -- Courthouse Witness Stand Left.
+			{ Coords = vector3(-1378.4508056640625, -629.0260620117188, 30.62794494628906), Radius = 1.5 }, -- Bahama Mamas.
 			{ Coords = vector3(4893.516, -4905.183, 3.486646), Radius = 2.0 }, -- Cayo Perico, Pleasure Cove.
 		},
 		Vehicles = {
