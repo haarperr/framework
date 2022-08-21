f67d27ef8a570977962253c1ac8870fce7243271
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-21 18:16:10 +0100

    Add Odin Station

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -264,6 +264,35 @@ Register("Stove", {
 	Model = "prop_cooker_03",
 })
 
+Register("Odin Station", {
+	Placement = "Floor",
+	NoCenter = true,
+	Station = {
+		Type = "Odin Station",
+		Auto = true,
+		Magnet = {
+			Offset = vector3(0, -1, 0),
+			Heading = 0.0,
+		},
+		Anim = {
+			In = {
+				Locked = true,
+				Sequence = {
+					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
+					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
+				},
+			},
+			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
+		},
+		Camera = {
+			Offset = vector3(-1.0, -1.0, 2.0),
+			Target = vector3(0.0, 0.0, 1.0),
+			Fov = 60.0,
+		},
+	},
+	Model = "prop_cooker_03",
+})
+
 Register("Processor", {
 	Stackable = {
 		Structure = true,
