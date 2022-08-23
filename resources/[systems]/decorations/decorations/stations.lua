f97946d5f7d491fe2a099a17662290432830a808
iamthevaleyard <iamvaleyard@gmail.com>
2022-08-23 05:15:12 +0100

    Update stations.lua

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -548,6 +548,35 @@ Register("Keycard Printer", {
 	Model = "prop_printer_01",
 })
 
+Register("Vault Keycard Printer", {
+	Placement = "Floor",
+	NoCenter = true,
+	Station = {
+		Type = "Vault Keycard Printer",
+		Auto = true,
+		Magnet = {
+			Offset = vector3(0, -1, 0),
+			Heading = 0.0,
+		},
+		Anim = {
+			In = {
+				Locked = true,
+				Sequence = {
+					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
+					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
+				},
+			},
+			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
+		},
+		Camera = {
+			Offset = vector3(-1.0, -1.0, 2.0),
+			Target = vector3(0.0, 0.0, 1.0),
+			Fov = 60.0,
+		},
+	},
+	Model = "prop_printer_01",
+})
+
 Register("Resource Sales", {
 	Placement = "Floor",
 	NoCenter = true,
