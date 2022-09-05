ff80e4933d01bdaf864037645dd208d8c8ae8568
Hikato <hikato@hikato.com>
2022-09-04 20:49:39 -0500

    (jobs): add OPA job

---
+++ resources/[jobs]/doj/sh_opa.lua
@@ -0,0 +1,43 @@
+exports.jobs:Register("opa", {
+	Title = "Federal",
+	Name = "Office of Public Administration",
+	Faction = "federal",
+	Pay = 100,
+	Group = "opa",
+	Clocks = {
+		{ Coords = vector3(-528.0028, -186.3415, 38.22709), Radius = 3.5 },
+	},
+	Tracker = {
+		Group = "emergency",
+		State = "opa",
+	},
+	Emergency = {
+		AccessMdt = "police",
+		CanBreach = true,
+		CanFine = true,
+		CanImpound = true,
+		CanJail = true,
+		CheckIn = 2,
+		ChopShop = false,
+		DrugBonus = false,
+		JailBreak = false,
+		Robberies = false,
+	},
+	Ranks = {
+		{ Name = "Administrator" },
+		{ Name = "Senior Administrator" },
+		{ Name = "Support Manager" },
+		{
+			Name = "Specialist",
+			Flags = Jobs.Permissions.ALL()
+		},
+		{
+			Name = "Deputy Director",
+			Flags = Jobs.Permissions.ALL()
+		},
+		{
+			Name = "Director",
+			Flags = Jobs.Permissions.ALL()
+		},
+	},
+})
