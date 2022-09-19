a9ff444640a352a409d7a794c7731275aa8979c1
Hikato <hikato@hikato.com>
2022-09-19 18:23:25 -0500

    (poledance): spacing and variable names wut

--- resources/[third-party]/poledance/cl_poledance.lua
+++ resources/[third-party]/poledance/cl_poledance.lua
@@ -1,22 +1,22 @@
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-			if Config['PoleDance']['Enabled'] then
-				for k, v in pairs(Config['PoleDance']['Locations']) do
-					if #(GetEntityCoords(PlayerPedId()) - v['Position']) <= 1.0 then
-						if IsControlJustReleased(0, 51) and not urmom then
-							urmom = true
-							LoadDict('mini@strip_club@pole_dance@pole_dance' .. v['Number'])
-							local scene = NetworkCreateSynchronisedScene(v['Position'], vector3(0.0, 0.0, 0.0), 2, false, true, 1065353216, 0, 1.3)
-							NetworkAddPedToSynchronisedScene(PlayerPedId(), scene, 'mini@strip_club@pole_dance@pole_dance' .. v['Number'], 'pd_dance_0' .. v['Number'], 1.5, -4.0, 1, 1, 1148846080, 0)
-							NetworkStartSynchronisedScene(scene)
-						elseif IsControlJustReleased(0, 51) and urmom then
-						urmom = false
+		if Config['PoleDance']['Enabled'] then
+			for k, v in pairs(Config['PoleDance']['Locations']) do
+				if #(GetEntityCoords(PlayerPedId()) - v['Position']) <= 1.0 then
+					if IsControlJustReleased(0, 51) and not Dancing then
+						DancingDancing = true
+						LoadDict('mini@strip_club@pole_dance@pole_dance' .. v['Number'])
+						local scene = NetworkCreateSynchronisedScene(v['Position'], vector3(0.0, 0.0, 0.0), 2, false, true, 1065353216, 0, 1.3)
+						NetworkAddPedToSynchronisedScene(PlayerPedId(), scene, 'mini@strip_club@pole_dance@pole_dance' .. v['Number'], 'pd_dance_0' .. v['Number'], 1.5, -4.0, 1, 1, 1148846080, 0)
+						NetworkStartSynchronisedScene(scene)
+					elseif IsControlJustReleased(0, 51) and Dancing then
+						Dancing = false
 						ClearPedTasksImmediately(PlayerPedId())
-						end
 					end
 				end
 			end
+		end
 	end
 end)
 
