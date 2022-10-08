ad1539477b57783dd2585de070c6fd21100027ba
Hikato <hikato@hikato.com>
2022-10-08 01:49:36 -0500

    (oldnpcs): lazy port of 1.0 NPC system for 2.0

---
+++ resources/[legacy]/oldnpcs/npcs/stations.lua
@@ -0,0 +1,33 @@
+-- Sandy SO
+AddNpc({
+	id = "ARMORY_GUARD_A",
+	coords = vector4(1859.8587646484375, 3690.260498046875, 34.27010345458984, 55.27508163452148),
+	model = "csb_trafficwarden",
+	idle = {
+		dict = "mini@strip_club@idles@bouncer@idle_c",
+		name = "idle_c",
+		flag = 1
+	},
+})
+-- MRPD
+AddNpc({
+	id = "ARMORY_GUARD_B",
+	coords = vector4(480.4213562011719, -996.6481323242188, 30.68980407714843, 88.0383529663086),
+	model = "s_f_y_cop_01",
+	idle = {
+		dict = "amb@world_human_cop_idles@male@idle_a",
+		name = "idle_c",
+		flag = 1
+	},
+})
+-- Highway PD
+AddNpc({
+	id = "ARMORY_GUARD_C",
+	coords = vector4(1549.97900390625, 838.8751831054688, 77.65585327148438, 149.18521118164065),
+	model = "s_m_y_cop_01",
+	idle = {
+		dict = "amb@world_human_cop_idles@male@idle_a",
+		name = "idle_c",
+		flag = 1
+	},
+})
