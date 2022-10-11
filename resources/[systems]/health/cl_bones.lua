0bbaef9a7a023563c547ebb2f57bb689abfc45c6
Hikato <hikato@hikato.com>
2022-10-11 08:54:54 -0500

    (health): enable passive healing when "okay"

--- resources/[systems]/health/cl_bones.lua
+++ resources/[systems]/health/cl_bones.lua
@@ -39,7 +39,7 @@ function Main:UpdateBones()
 	if not Injury.isDead and BloodLoss < 0.001 and (self:GetEffect("Health") or 1.0) < 0.999 then
 		local healAmount = (1.0 / 300.0 * deltaTime) * ((self:GetEffect("Comfort") or 0.0) * 3.0 + 1.0)
 		--print("healing health", healAmount)
-		-- self:AddEffect("Health", healAmount)
+		self:AddEffect("Health", healAmount)
 	end
 
 	-- Update snowflake.
