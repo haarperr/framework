fb9134936606ac5371723f1331317404c0d6ad5e
Hikato <hikato@hikato.com>
2022-10-15 23:44:06 -0500

    Revert "Revert "Revert "Additional Fish stations and Grinder station fix"""
    This reverts commit d6b684e78eb67960fa385c04e1ae10e113e60026.

--- resources/[systems]/decorations/decorations/stations.lua
+++ resources/[systems]/decorations/decorations/stations.lua
@@ -635,64 +635,6 @@ Register("Fish Sales", {
 	Model = "v_res_fa_chopbrd",
 })
 
-Register("Deep Water Fish Sales", {
-	Placement = "Floor",
-	NoCenter = true,
-	Station = {
-		Type = "Deep Water Fish Sales",
-		Auto = true,
-		Magnet = {
-			Offset = vector3(0, -1, 0),
-			Heading = 0.0,
-		},
-		Anim = {
-			In = {
-				Locked = true,
-				Sequence = {
-					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
-					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
-				},
-			},
-			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
-		},
-		Camera = {
-			Offset = vector3(-1.0, -1.0, 2.0),
-			Target = vector3(0.0, 0.0, 1.0),
-			Fov = 60.0,
-		},
-	},
-	Model = "v_res_fa_chopbrd",
-})
-
-Register("Illegal Fish Sales", {
-	Placement = "Floor",
-	NoCenter = true,
-	Station = {
-		Type = "Illegal Fish Sales",
-		Auto = true,
-		Magnet = {
-			Offset = vector3(0, -1, 0),
-			Heading = 0.0,
-		},
-		Anim = {
-			In = {
-				Locked = true,
-				Sequence = {
-					{ Dict = "amb@prop_human_bbq@male@enter", Name = "enter", Flag = 0, Locked = true },
-					{ Dict = "amb@prop_human_bbq@male@idle_a", Name = "idle_b", Flag = 1, Locked = true },
-				},
-			},
-			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
-		},
-		Camera = {
-			Offset = vector3(-1.0, -1.0, 2.0),
-			Target = vector3(0.0, 0.0, 1.0),
-			Fov = 60.0,
-		},
-	},
-	Model = "v_res_fa_chopbrd",
-})
-
 Register("Meat Sales", {
 	Placement = "Floor",
 	NoCenter = true,
@@ -918,7 +860,7 @@ Register("Grindstone", {
 			Out = { Dict = "amb@prop_human_bbq@male@exit", Name = "exit", Flag = 0, BlendIn = 100.0 },
 		},
 		Camera = {
-			Offset = vector3(-0.5, -0.5, 2.0),
+			Offset = vector3(-1.0, -1.0, 2.0),
 			Target = vector3(0.0, 0.0, 1.0),
 			Fov = 60.0,
 		},
