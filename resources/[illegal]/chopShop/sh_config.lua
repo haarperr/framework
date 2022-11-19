182562d05d64858646bcc9f3b4642984b7b4f007
iParadoxi <91035924+iParadoxi@users.noreply.github.com>
2022-11-19 13:22:18 -0800

    Revert "Revert "Revert "Revert "(chopShop): port 1.0 changes, add chance system for payouts""""
    This reverts commit 901806fc25bba36f68ee09b17c1367a3a4b4ac06.

--- resources/[illegal]/chopShop/sh_config.lua
+++ resources/[illegal]/chopShop/sh_config.lua
@@ -1,19 +1,112 @@
 Config = {
 	CarsPerList = { Min = 15, Max = 20 },
-	ListDuration = { Min = 30, Max = 40 },
-	Presence = { Min = 3, Max = 10, MaxRate = 1.5 },
+	ListDuration = { Min = 45, Max = 60 },
+	Presence = { Min = 0, Max = 8, MaxRate = 1.5 },
 	Components = {
-		{ Name = "engine", Type = -1, Index = 0, Offset = vector3(0, 0, -0.5) },
-		{ Name = "door_dside_f", Type = 0, Index = 0, Offset = vector3(0.5, 0.5, 0) },
-		{ Name = "door_dside_r", Type = 0, Index = 2, Offset = vector3(0.5, 0.5, 0) },
-		{ Name = "door_pside_f", Type = 0, Index = 1, Offset = vector3(-0.5, 0.5, 0) },
-		{ Name = "door_pside_r", Type = 0, Index = 3, Offset = vector3(-0.5, 0.5, 0) },
-		{ Name = "bonnet", Type = 0, Index = 4, Offset = vector3(0, -1.5, 0) },
-		{ Name = "boot", Type = 0, Index = 5, Offset = vector3(0, 1.0, 0) },
-		{ Name = "wheel_lf", Type = 1, Index = 0, Offset = vector3(0.5, 0, 0) },
-		{ Name = "wheel_lr", Type = 1, Index = 4, Offset = vector3(0.5, 0, 0) },
-		{ Name = "wheel_rf", Type = 1, Index = 1, Offset = vector3(-0.5, 0, 0) },
-		{ Name = "wheel_rr", Type = 1, Index = 5, Offset = vector3(-0.5, 0, 0) },
+		{
+			Name = "engine",
+			Type = -1,
+			Index = 0,
+			Offset = vector3(0, 0, -0.5),
+			Items = { -- Lower chance items go at the top.
+				["Iron Ingot"] = { Amount = {0, 0}, Chance = 0.2 },
+				["Scrap Metal"] = { Amount = {0, 0}, Chance = 1.0 },
+			}
+		},
+		{
+			Name = "door_dside_f",
+			Type = 0,
+			Index = 0,
+			Offset = vector3(0.5, 0.5, 0),
+			Items = {
+				["Polymer Plastic"] = { Amount = {1, 1}, Chance = 0.2 },
+				["Scrap Metal"] = { Amount = {1, 1}, Chance = 1.0 },
+			}
+		},
+		{
+			Name = "door_dside_r",
+			Type = 0,
+			Index = 2,
+			Offset = vector3(0.5, 0.5, 0),
+			Items = {
+				["Polymer Plastic"] = { Amount = {1, 1}, Chance = 0.2 },
+				["Scrap Metal"] = { Amount = {1, 1}, Chance = 1.0 },
+			}
+		},
+		{
+			Name = "door_pside_f",
+			Type = 0,
+			Index = 1,
+			Offset = vector3(-0.5, 0.5, 0),
+			Items = {
+				["Polymer Plastic"] = { Amount = {1, 1}, Chance = 0.2 },
+				["Scrap Metal"] = { Amount = {1, 1}, Chance = 1.0 },
+			}
+		},
+		{
+			Name = "door_pside_r",
+			Type = 0,
+			Index = 3,
+			Offset = vector3(-0.5, 0.5, 0),
+			Items = {
+				["Polymer Plastic"] = { Amount = {1, 1}, Chance = 0.2 },
+				["Scrap Metal"] = { Amount = {1, 1}, Chance = 1.0 },
+		 	}
+		},
+		{
+			Name = "bonnet",
+			Type = 0,
+			Index = 4,
+			Offset = vector3(0, -1.5, 0),
+			Items = {
+				["Scrap Metal"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
+		{
+			Name = "boot",
+			Type = 0,
+			Index = 5,
+			Offset = vector3(0, 1.0, 0),
+			Items = {
+				["Polymer Plastic"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
+		{
+			Name = "wheel_lf",
+			Type = 1,
+			Index = 0,
+			Offset = vector3(0.5, 0, 0),
+			Items = {
+				["Scrap Rubber"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
+		{
+			Name = "wheel_lr",
+			Type = 1,
+			Index = 4,
+			Offset = vector3(0.5, 0, 0),
+			Items = {
+				["Scrap Rubber"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
+		{
+			Name = "wheel_rf",
+			Type = 1,
+			Index = 1,
+			Offset = vector3(-0.5, 0, 0),
+			Items = {
+				["Scrap Rubber"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
+		{
+			Name = "wheel_rr",
+			Type = 1,
+			Index = 5,
+			Offset = vector3(-0.5, 0, 0),
+			Payout = {
+				["Scrap Rubber"] = { Amount = {0, 0}, Chance = 0.5 },
+			}
+		},
 		-- { Name = "wheel_lm1", Type = 1, Index = 0 },
 		-- { Name = "wheel_lm2", Type = 1, Index = 0 },
 		-- { Name = "wheel_lm3", Type = 1, Index = 0 },
@@ -26,7 +119,7 @@ Config = {
 		--[[ Chop Zones ]]--
 		-- Hayes Auto.
 		-- { Type = 0, Visibility = 10.0, Radius = 5.0, Chance = 100, Coords = vector3(481.17077636719, -1317.1510009766, 29.202482223511) },
-		
+
 		-- Davis Auto Repairs.
 		{ Type = 0, Visibility = 10.0, Radius = 5.0, Chance = 100, Coords = vector3(158.90139770507812, -1694.040771484375, 29.44917488098145) },
 
@@ -54,100 +147,124 @@ Config = {
 		{ Type = 1, Visibility = 50.0, Radius = 1.5, Chance = 50, Coords = vector3(-542.38568115234, -1636.5914306641, 19.062301635742), Peds = {
 			{ Model = "g_m_m_armboss_01" },
 		}, Heading = 94.182029724121},
-		
+
 		-- La Puerta Garbage Disposal.
 		{ Type = 1, Visibility = 50.0, Radius = 1.5, Chance = 50, Coords = vector3(-332.68402099609, -1554.4663085938, 27.718605041504), Peds = {
 			{ Model = "cs_brad" },
 		}, Heading = 324.99084472656},
-		
+
 		-- Boneyard.
 		{ Type = 1, Visibility = 50.0, Radius = 1.5, Chance = 75, Coords = vector3(2401.2541503906, 3124.6650390625, 48.152935028076), Peds = {
 			{ Model = "ig_cletus" },
 		}, Heading = 204.67668151855},
-		
+
 		-- Joshua Road.
 		{ Type = 1, Visibility = 50.0, Radius = 1.5, Chance = 50, Coords = vector3(183.25653076172, 2791.0148925781, 45.603225708008), Peds = {
 			{ Model = "s_m_y_construct_01" },
 		}, Heading = 312.94845581055},
-		
+
 		-- Senora Road Gas Station.
 		{ Type = 1, Visibility = 50.0, Radius = 1.5, Chance = 50, Coords = vector3(264.29803466797, 2591.90625, 45.0563621521), Peds = {
 			{ Model = "s_m_m_autoshop_02" },
 		}, Heading = 164.7827911377},
 	},
 	Cars = {
-		{ Model = "adder", BasePrice = 100, Chance = 10 },
-		{ Model = "akuma", BasePrice = 40, Chance = 80 },
-		{ Model = "alpha", BasePrice = 75, Chance = 30 },
-		{ Model = "asea", BasePrice = 80, Chance = 50 },
-		{ Model = "bagger", BasePrice = 50, Chance = 90 },
-		{ Model = "banshee", BasePrice = 75, Chance = 10 },
-		{ Model = "banshee2", BasePrice = 65, Chance = 10 },
-		{ Model = "bati", BasePrice = 60, Chance = 30 },
-		{ Model = "bifta", BasePrice = 50, Chance = 50 },
-		{ Model = "bjxl", BasePrice = 50, Chance = 50 },
-		{ Model = "buffalo", BasePrice = 75, Chance = 90 },
-		{ Model = "bullet", BasePrice = 92, Chance = 10 },
-		{ Model = "caddy", BasePrice = 40, Chance = 20 },
-		{ Model = "caddy2", BasePrice = 40, Chance = 20 },
-		{ Model = "coquette", BasePrice = 92, Chance = 20 },
-		{ Model = "dilettante", BasePrice = 40, Chance = 50 },
-		{ Model = "dubsta", BasePrice = 45, Chance = 40 },
-		{ Model = "dubsta2", BasePrice = 45, Chance = 40 },
-		{ Model = "enduro", BasePrice = 50, Chance = 40 },
-		{ Model = "esskey", BasePrice = 50, Chance = 45 },
-		{ Model = "f620", BasePrice = 50, Chance = 40 },
-		{ Model = "faction", BasePrice = 50, Chance = 50 },
-		{ Model = "felon", BasePrice = 50, Chance = 30 },
-		{ Model = "feltzer2", BasePrice = 65, Chance = 85 },
-		{ Model = "fusilade", BasePrice = 65, Chance = 20 },
+		--{ Model = "adder", BasePrice = 500, Chance = 10 },
+		{ Model = "akuma", BasePrice = 150, Chance = 80 },
+		{ Model = "alpha", BasePrice = 187, Chance = 30 },
+		{ Model = "asea", BasePrice = 150, Chance = 50 },
+		{ Model = "bagger", BasePrice = 150, Chance = 90 },
+		{ Model = "banshee", BasePrice = 187, Chance = 10 },
+		{ Model = "banshee2", BasePrice = 187, Chance = 10 },
+		{ Model = "bati", BasePrice = 150, Chance = 30 },
+		{ Model = "bifta", BasePrice = 150, Chance = 50 },
+		{ Model = "bjxl", BasePrice = 150, Chance = 50 },
+		{ Model = "buffalo", BasePrice = 187, Chance = 90 },
+		{ Model = "bullet", BasePrice = 262, Chance = 10 },
+		{ Model = "caddy", BasePrice = 150, Chance = 20 },
+		{ Model = "caddy2", BasePrice = 150, Chance = 20 },
+		{ Model = "coquette", BasePrice = 225, Chance = 20 },
+		{ Model = "dilettante", BasePrice = 150, Chance = 50 },
+		{ Model = "dubsta", BasePrice = 187, Chance = 40 },
+		{ Model = "dubsta2", BasePrice = 225, Chance = 40 },
+		{ Model = "enduro", BasePrice = 150, Chance = 40 },
+		{ Model = "esskey", BasePrice = 150, Chance = 45 },
+		{ Model = "f620", BasePrice = 150, Chance = 40 },
+		{ Model = "faction", BasePrice = 150, Chance = 50 },
+		{ Model = "felon", BasePrice = 150, Chance = 30 },
+		{ Model = "feltzer2", BasePrice = 225, Chance = 85 },
+		{ Model = "fusilade", BasePrice = 187, Chance = 20 },
 		{ Model = "glendale", BasePrice = 150, Chance = 50 },
-		{ Model = "granger", BasePrice = 40, Chance = 50 },
-		{ Model = "habanero", BasePrice = 40, Chance = 50 },
-		{ Model = "issi2", BasePrice = 40, Chance = 50 },
-		{ Model = "jester", BasePrice = 55, Chance = 40 },
-		{ Model = "jester2", BasePrice = 60, Chance = 40 },
-		{ Model = "kalahari", BasePrice = 70, Chance = 50 },
-		{ Model = "massacro", BasePrice = 65, Chance = 30 },
-		{ Model = "mesa", BasePrice = 50, Chance = 45 },
-		{ Model = "mesa3", BasePrice = 50, Chance = 50 },
-		{ Model = "mower", BasePrice = 50, Chance = 20 },
-		{ Model = "ninef", BasePrice = 65, Chance = 20 },
-		{ Model = "ninef2", BasePrice = 62, Chance = 25 },
-		{ Model = "oracle", BasePrice = 40, Chance = 50 },
-		{ Model = "panto", BasePrice = 50, Chance = 50 },
-		{ Model = "patriot", BasePrice = 80, Chance = 60 },
-		{ Model = "phoenix", BasePrice = 80, Chance = 50 },
-		{ Model = "premier", BasePrice = 50, Chance = 50 },
-		{ Model = "rebel", BasePrice = 50, Chance = 40 },
-		{ Model = "rebel2", BasePrice = 50, Chance = 50 },
-		{ Model = "regina", BasePrice = 80, Chance = 50 },
-		{ Model = "rentalbus", BasePrice = 80, Chance = 70 },
-		{ Model = "rhapsody", BasePrice = 50, Chance = 50 },
-		{ Model = "rocoto", BasePrice = 50, Chance = 50 },
-		{ Model = "sabregt", BasePrice = 65, Chance = 60 },
-		{ Model = "sandking", BasePrice = 50, Chance = 50 },
-		{ Model = "sandking2", BasePrice = 50, Chance = 40 },
-		{ Model = "schafter2", BasePrice = 65, Chance = 50 },
-		{ Model = "schafter3", BasePrice = 50, Chance = 20 },
-		{ Model = "schafter4", BasePrice = 40, Chance = 30 },
-		{ Model = "seminole", BasePrice = 50, Chance = 35 },
-		{ Model = "sentinel", BasePrice = 65, Chance = 50 },
-		{ Model = "sentinel2", BasePrice = 85, Chance = 100 },
-		{ Model = "sovereign", BasePrice = 40, Chance = 80 },
-		{ Model = "speedo", BasePrice = 40, Chance = 70 },
-		{ Model = "stretch", BasePrice = 75, Chance = 20 },
-		{ Model = "sultan", BasePrice = 85, Chance = 80 },
-		{ Model = "superd", BasePrice = 75, Chance = 50 },
-		{ Model = "taco", BasePrice = 30, Chance = 70 },
-		{ Model = "tourbus", BasePrice = 50, Chance = 70 },
-		{ Model = "turismor", BasePrice = 60, Chance = 10 },
-		{ Model = "voltic", BasePrice = 60, Chance = 10 },
-		{ Model = "voodoo", BasePrice = 50, Chance = 60 },
-		{ Model = "washington", BasePrice = 50, Chance = 70 },
-		{ Model = "xls", BasePrice = 50, Chance = 45 },
-		{ Model = "youga", BasePrice = 50, Chance = 60 },
-		{ Model = "zentorno", BasePrice = 120, Chance = 10 },
+		{ Model = "granger", BasePrice = 150, Chance = 50 },
+		{ Model = "habanero", BasePrice = 150, Chance = 50 },
+		{ Model = "issi2", BasePrice = 150, Chance = 50 },
+		{ Model = "jester", BasePrice = 225, Chance = 40 },
+		{ Model = "jester2", BasePrice = 300, Chance = 40 },
+		{ Model = "kalahari", BasePrice = 150, Chance = 50 },
+		{ Model = "massacro", BasePrice = 225, Chance = 30 },
+		{ Model = "mesa", BasePrice = 150, Chance = 45 },
+		{ Model = "mesa3", BasePrice = 150, Chance = 50 },
+		{ Model = "mower", BasePrice = 150, Chance = 20 },
+		{ Model = "ninef", BasePrice = 225, Chance = 20 },
+		{ Model = "ninef2", BasePrice = 262, Chance = 25 },
+		{ Model = "oracle", BasePrice = 150, Chance = 50 },
+		{ Model = "panto", BasePrice = 150, Chance = 50 },
+		{ Model = "patriot", BasePrice = 150, Chance = 60 },
+		{ Model = "phoenix", BasePrice = 150, Chance = 50 },
+		{ Model = "premier", BasePrice = 150, Chance = 50 },
+		{ Model = "rebel", BasePrice = 150, Chance = 40 },
+		{ Model = "rebel2", BasePrice = 150, Chance = 50 },
+		{ Model = "regina", BasePrice = 150, Chance = 50 },
+		{ Model = "rentalbus", BasePrice = 150, Chance = 70 },
+		{ Model = "rhapsody", BasePrice = 150, Chance = 50 },
+		{ Model = "rocoto", BasePrice = 150, Chance = 50 },
+		{ Model = "sabregt", BasePrice = 225, Chance = 60 },
+		{ Model = "sandking", BasePrice = 150, Chance = 50 },
+		{ Model = "sandking2", BasePrice = 150, Chance = 40 },
+		{ Model = "schafter2", BasePrice = 225, Chance = 50 },
+		{ Model = "schafter3", BasePrice = 180, Chance = 20 },
+		{ Model = "schafter4", BasePrice = 180, Chance = 30 },
+		{ Model = "seminole", BasePrice = 150, Chance = 35 },
+		{ Model = "sentinel", BasePrice = 225, Chance = 100 },
+		{ Model = "sentinel2", BasePrice = 225, Chance = 100 },
+		{ Model = "sovereign", BasePrice = 150, Chance = 80 },
+		{ Model = "speedo", BasePrice = 150, Chance = 70 },
+		{ Model = "stretch", BasePrice = 225, Chance = 20 },
+		{ Model = "sultan", BasePrice = 185, Chance = 80 },
+		{ Model = "superd", BasePrice = 375, Chance = 50 },
+		{ Model = "taco", BasePrice = 150, Chance = 70 },
+		{ Model = "tourbus", BasePrice = 150, Chance = 70 },
+		{ Model = "turismor", BasePrice = 300, Chance = 10 },
+		{ Model = "voltic", BasePrice = 300, Chance = 10 },
+		{ Model = "voodoo", BasePrice = 150, Chance = 60 },
+		{ Model = "washington", BasePrice = 150, Chance = 70 },
+		{ Model = "xls", BasePrice = 150, Chance = 45 },
+		{ Model = "youga", BasePrice = 150, Chance = 60 },
+		--{ Model = "zentorno", BasePrice = 500, Chance = 10 },
+	},
+	Classes = {
+		[0] = 1, -- Compacts.
+		[1] = 1.5, -- Sedans.
+		[2] = 2, -- SUVs.
+		[3] = 1.5, -- Coupes.
+		[4] = 2, -- Muscle.
+		[5] = 2, -- Sports Classics.
+		[6] = 1.5, -- Sports.
+		[7] = 3, -- Super.
+		[8] = 1, -- Motorcycles.
+		[9] = 2, -- Off-road.
+		[10] = 2.5, -- Industrial.
+		[11] = 2.5, -- Utility.
+		[12] = 2, -- Vans.
+		[13] = 1, -- Cycles.
+		[14] = 1, -- Boats.
+		[15] = 4, -- Helicopters.
+		[16] = 0, -- Planes.
+		[17] = 3, -- Service.
+		[18] = 4, -- Emergency.
+		[19] = 0, -- Military.
+		[20] = 2.5, -- Commercial.
+		[21] = 5, -- Trains.
 	},
 	-- The object informants hold.
 	HeldObject = {
