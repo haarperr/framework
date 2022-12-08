76de4d4cb635110aaeda6927a095aa8fd7121ca7
FFMangTTV <77847001+FFMangTTV@users.noreply.github.com>
2022-12-08 09:16:48 -0600

    Removing Chassis from modkit because Armor Plating.

--- resources/[systems]/vehicles/modding/cl_mods.lua
+++ resources/[systems]/vehicles/modding/cl_mods.lua
@@ -56,7 +56,7 @@ Mods = {
 		[41] = { Name = "Struts" }, -- VMT_ENGINEBAY3
 		[42] = { Name = "Arch Cover" }, -- VMT_CHASSIS2
 		[43] = { Name = "Aerials" }, -- VMT_CHASSIS3
-		[44] = { Name = "Trim" }, -- VMT_CHASSIS4
+		[44] = { Name = "Trim", Hidden = true }, -- VMT_CHASSIS4
 		[45] = { Name = "Tank", Hidden = true  }, -- VMT_CHASSIS5
 		[46] = { Name = "Windows" }, -- VMT_DOOR_L
 		[47] = { Name = "Windows 2" }, -- VMT_DOOR_R
